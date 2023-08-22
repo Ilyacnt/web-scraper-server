d6256aee4c24e1bf1f2e822060d5b8a21f00c096
Ilyacnt <ilyagt2012@gmail.com>
2023-08-22 13:38:02 +0300

    Little parser refactoring and min price parsing

--- src/scraping/HeadlessParser.ts
+++ src/scraping/HeadlessParser.ts
@@ -1,11 +1,41 @@
 import puppeteer, { Browser, Page } from 'puppeteer'
 
+export const enum CategorySortBy {
+    PRICE_ASC = 'price_asc',
+    PRICE_DESC = 'price_desc',
+    DISCOUNT_DESC = 'discount_desc',
+}
+
+type DataFromPage = 'price' | 'discount'
+
 class HeadlessParser {
     public browserInstance: Browser
-    public url: string = 'https://hoff.ru/catalog/gostinaya/divany/?sort=price_asc'
+    // public url: string = 'https://hoff.ru/catalog/gostinaya/divany/?sort=price_asc'
+    public url: string = process.env.HOFF_URL || 'https://hoff.ru/'
 
     constructor() {
         this.createBrowser()
+    }
+
+    public async parseMinPrice(
+        category: string,
+        sortBy: CategorySortBy
+    ): Promise<Record<string, string>> {
+        return await this.openHoffPageByCategory(category, sortBy)
+    }
+
+    private async openHoffPageByCategory(
+        category: string,
+        sortBy: CategorySortBy
+    ): Promise<Record<string, string>> {
+        let currentUrl = this.url + `catalog/${category}/?sort=${sortBy}`
+
+        let page = await this.browserInstance.newPage()
+        console.log(`Navigating to ${currentUrl}...`)
+        await page.goto(currentUrl)
+
+        const result = await this.selectDataFromPage(page)
+        return result
     }
 
     private async createBrowser(): Promise<void> {
@@ -23,26 +53,28 @@ class HeadlessParser {
         }
     }
 
-    private async openHoffPage(): Promise<void> {
-        let page = await this.browserInstance.newPage()
-        console.log(`Navigating to ${this.url}...`)
-        await page.goto(this.url)
+    private async selectDataFromPage(
+        page: Page,
+        dataType: DataFromPage = 'price'
+    ): Promise<Record<string, string>> {
+        let result = {}
+        if (dataType === 'price') {
+            await page.waitForSelector('.current-price')
 
-        await this.selectDataFromPage(page)
-    }
+            result = await page.$$eval('.current-price', (elements) => {
+                return { minPrice: elements[0].textContent }
+            })
+        } else if (dataType === 'discount') {
+            await page.waitForSelector('.promo-label__text')
 
-    private async selectDataFromPage(page: Page): Promise<void> {
-        await page.waitForSelector('.current-price')
-
-        let result = await page.$$eval('.current-price', (elements) => {
-            return elements[0].textContent
-        })
+            result = await page.$$eval('.promo-label__text', (elements) => {
+                return { maxDiscount: elements[0].textContent }
+            })
+        }
 
         console.log(result)
-    }
 
-    async test(): Promise<void> {
-        await this.openHoffPage()
+        return result
     }
 }
 
